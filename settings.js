54959120c521dc00ed55edcbbd0c8405c9eec38b
unknown <syrenthaofficial@gmail.com>
2025-09-05 06:17:53 +0100

    Initial project upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Sყɾҽɳƚԋα',
+  author: '‎',
+  botName: "Sყɾҽɳƚԋα",
+  botOwner: '𝓕𝓪𝓲𝔃𝓪𝓷', // Your name
+  ownerNumber: '923226070203', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.1.6",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
